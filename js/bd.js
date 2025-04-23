f3ecece7b95f800384e1c3d1300bfdf29859bdd8
Leticia <leticia.silva2374@etec.sp.gov.br>
2025-04-23 11:16:55 -0300

    primeiro commit

---
+++ js/bd.js
@@ -0,0 +1,61 @@
+const memorias = [
+    {
+      "titulo": "Nossa foto em frente a Igreja",
+      "img": "./Fotos/Goiania_Igreja.jpeg",
+      "descricao": "Às vezes, a vida nos dá momentos que são simples, mas que são tudo. E esses momentos são os que levamos conosco para sempre. — The Secret Life of Walter Mitty"
+    },
+    {
+      "titulo": "Naquela praça que a gente amava",
+      "img": "./Fotos/Goiania_3.jpeg",
+      "descricao": "Alguns dos melhores momentos da vida são aqueles que não são planejados, apenas acontecem. Como um simples passeio, uma conversa ou um açaí na praça. — Amor e Outras Drogas"
+    },
+    {
+      "titulo": "Indo viajar",
+      "img": "./Fotos/Praia.jpeg",
+      "descricao": "Às vezes, as melhores lembranças não são feitas no tempo planejado, mas sim nos momentos em que paramos para aproveitar a companhia e o carinho de quem está ao nosso lado. — Incrível!"
+    },
+    {
+      "titulo": "Minha foto favorita",
+      "img": "./Fotos/Shopping.jpeg",
+      "descricao": "Os melhores dias não são aqueles que têm a maior aventura, mas sim aqueles em que estamos cercados de quem amamos, compartilhando risos e criando memórias que durarão para sempre.  — A Vida é Bela"
+    },
+    {
+      "titulo": "Sua foto de perfil",
+      "img": "./Fotos/Perfil.jpeg",
+      "descricao": "A vida passa num piscar de olhos. Aproveite os momentos, tire a foto, mesmo que alguém reclame. Um dia, tudo isso vira saudade boa. — This Is Us" 
+    },
+    {
+      "titulo": "Primeira vez na Canção Nova",
+      "img": "./Fotos/Cançao nova.jpeg",
+      "descricao": "Foi ali, naquele momento, que entendi: Deus nunca esteve distante. Era eu que precisava me aproximar. Às vezes, tudo o que o coração precisa é de um lugar de paz para lembrar que nunca esteve sozinho."
+    },
+    {
+      "titulo": "Na casa da mãezinha",
+      "img": "./Fotos/Aparecida.jpeg",
+      "descricao":  "Às vezes, tudo que precisamos é voltar às nossas raízes para lembrar onde está a nossa força. — The Chosen" 
+    },
+    {
+      "titulo": "Minha formatura",
+      "img": "./Fotos/Formatura.jpg",
+      "descricao":  "O que torna um dia especial não é o que acontece, mas quem está lá pra viver aquilo com você. — Glee" 
+    },
+    {
+      "titulo": "Quatro patas",
+      "img": "./Fotos/Flechinha.jpg",
+      "descricao": "Alguns anjos não têm asas… têm quatro patas, bagunçam tudo e nos ensinam o que é amor incondicional. — Marley & Eu" 
+    },
+
+    {
+      "titulo": "Presente de Deus",
+      "img": "./Fotos/Ursulinha.jpg",
+      "descricao": "Ela chegou e, com um simples sorriso, ensinou que o amor pode ser pequenininho no tamanho, mas imenso no coração. — Dumbo (Disney)" 
+    },
+
+    {
+      "titulo": "Nossa família",
+      "img": "./Fotos/Familia.jpg",
+      "descricao": "A família não é sobre quem está ao seu lado o tempo inteiro, mas sobre quem, mesmo distante, está com você em cada momento, em cada lembrança, e no amor que nunca se vai. Às vezes, aqueles que amamos se afastam fisicamente, mas suas presenças se tornam parte do que somos, e isso é para sempre. — Vingadores: Ultimato" 
+    }
+  ]
+
+  export {memorias}
